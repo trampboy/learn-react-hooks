772960b32536178bbc22794dd07c437e5de60262
Jing Hong Gang <honggangjing@gmail.com>
2022-12-16 18:26:11 +0800

    feat: 01｜认识 React：如何创建你的第一个 React 应用

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -1,5 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function UserList() {
+  const [users, setUsers] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
+
+  const fetchUsers = async ()  => {
+    setLoading(true)
+    try {
+      const res = await fetch("https://reqres.in/api/users/")
+      const json = await res.json()
+      setUsers(json.data)
+    } catch (err) {
+      setError(err)
+    }
+    setLoading(false)
+  }
+
+  return (
+    <div>
+      <button onClick={fetchUsers} disabled={loading}>
+        {loading ? "Loading..." : "Show Users"}
+      </button>
+      {error && <div>Failed: {String(error)}</div>}
+      <br/>
+      <ul>
+        {users.length > 0 && users.map((user) => {
+          return <li key={user.id}>{user.first_name}</li>
+        })}
+      </ul>
+    </div>
+
+  )
+}
 
 function App() {
   return (
@@ -17,6 +52,7 @@ function App() {
         >
           Learn React
         </a>
+        <UserList/>
       </header>
     </div>
   );
